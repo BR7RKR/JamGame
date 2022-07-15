7d22462baadf6528396b9bd5a3094c0fbeb69336
Lev Myts Yaroslavovich <leomyts@gmail.com>
2022-07-15 23:37:48 +0300

    Написал скрипт для движения

--- JamGame/Assets/Scripts/MovePlayer.cs
+++ JamGame/Assets/Scripts/MovePlayer.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _horizontalInput;
+    private float _verticalInput;
+    
+    [Tooltip("Settings")]
+    [SerializeField]private float _velocity = 10f;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
         
+        transform.Translate(Vector3.right.normalized * _horizontalInput * _velocity * Time.deltaTime);
+        transform.Translate(Vector3.forward.normalized * _verticalInput * _velocity * Time.deltaTime);
     }
 }
