7a803c727e707f491ecd8e2779bbb0b36019e22f
thefireKS <KS29092003@gmail.com>
2022-07-17 12:50:39 +0300

    Swap weapon animation when swapping weapon

--- JamGame/Assets/Scripts/PlayersArmory.cs
+++ JamGame/Assets/Scripts/PlayersArmory.cs
@@ -1,19 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayersArmory : MonoBehaviour
 {
+    [SerializeField] private MovePlayer plmv;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float reloadCooldown = 0f;
+    private int reloadsCount = 1;
     public GameObject[] bullets;
+    private WaitForSeconds cd;
 
+    private void Awake()
+    {
+        cd = new WaitForSeconds(1.66f);
+        plmv = GetComponent<MovePlayer>();
+    }
+
+    void Update()
+    {
+        reloadCooldown += Time.deltaTime;
+        if (reloadCooldown >= 15f)
+        {
+            reloadCooldown = 0f;
+            if (reloadsCount < 3)
+                reloadsCount++;
+        }
+    }
     public GameObject DiceRoll()
     {
         int max = bullets.Length;
         GameObject ShootingBullet = null;
         if (bullets != null)
         {
-            ShootingBullet = bullets[Random.Range(0,max)];
+            if (reloadsCount > 0)
+            {
+                StartCoroutine(AnimationSwapping());
+                ShootingBullet = bullets[Random.Range(0,max)];
+                reloadsCount--;
+            }
         }
         return ShootingBullet;
+    }
+
+    private IEnumerator AnimationSwapping()
+    {
+        plmv.enabled = false;
+        anim.SetBool("IsMoving",false);
+        anim.SetBool("IsReloading",true);
+        yield return cd;
+        anim.SetBool("IsReloading",false);
+        plmv.enabled = true;
     }
 }
